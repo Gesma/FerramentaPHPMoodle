25a1df852f4bf55fcd6858052638b7479331d06c
FelipeNBrito <felipenevesbrito@gmail.com>
2015-05-17 16:17:33 -0300

    Codigo migrado

---
+++ messages/_delete_message.php
@@ -0,0 +1,17 @@
+
+<?php
+
+        $id = $_REQUEST['id'];
+
+        include_once "../database.php";
+
+        $db = new Database();
+
+        $sql = "DELETE FROM ag_mensagens WHERE id = $id";
+
+        $res = $db->exec($sql);
+
+        if($res) echo 0;
+        else echo 1;
+
+?>
